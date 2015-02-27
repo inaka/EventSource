c5ccef7f6a315b110a620c80033cda3a1070f8db
Andres Canal <andres@inakanetworks.com>
2015-02-27 17:22:53 -0300

    started working on readme and added more stuff to the example to create a gif for the readme

--- EventSourceExample/ViewController.swift
+++ EventSourceExample/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var status: UILabel!
+    @IBOutlet var dataLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var idLabel: UILabel!
+    @IBOutlet var squareConstraint: NSLayoutConstraint!
     var eventSource: EventSource?
 
     override func viewDidLoad() {
@@ -22,28 +27,59 @@ class ViewController: UIViewController {
         let basicAuthAuthorization = EventSource.basicAuth(username, password: password)
         
 
-//        eventSource = EventSource(url: server, headers: ["Authorization" : basicAuthAuthorization])
+        eventSource = EventSource(url: server, headers: ["Authorization" : basicAuthAuthorization])
         
         eventSource?.onOpen {
-            println("onOpen");
+            self.status.backgroundColor = UIColor.greenColor()
+            self.status.text = "Connected"
         }
         
         eventSource?.onError { (error) in
-            if let currentError = error{
-                print(currentError)
-            }else{
-                print("Error")
-            }
+            self.status.backgroundColor = UIColor.redColor()
+            self.status.text = "Disconnected"
         }
 
         eventSource?.onMessage { (id, event, data) in
-            println(id)
-            println(event)
-            println(data)
-            println()
+            self.updateLabels(id, event: event, data: data)
+        }
+        
+        eventSource?.addEventListener("event-name") { (id, event, data) in
+            self.updateLabels(id, event: event, data: data)
         }
         
 //        eventSource.close()
+    }
+    
+    func updateLabels(id: String?, event: String?, data: String?) -> Void{
+        self.idLabel.text = ""
+        self.idLabel.text = ""
+        self.idLabel.text = ""
+        
+        if let eventID = id {
+            self.idLabel.text = id
+        }
+        
+        if let eventName = event {
+            self.nameLabel.text = eventName
+        }
+        
+        if let eventData = data {
+            self.dataLabel.text = eventData
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let finalPosition = self.view.frame.size.width - 50
+        
+        self.squareConstraint.constant = 0
+        self.view.layoutIfNeeded()
+        
+        UIView.animateKeyframesWithDuration(2, delay: 0, options: UIViewKeyframeAnimationOptions.Repeat | UIViewKeyframeAnimationOptions.Autoreverse , animations: { () in
+            self.squareConstraint.constant = finalPosition
+            self.view.layoutIfNeeded()            
+        }, completion: nil)
     }
 }
 
