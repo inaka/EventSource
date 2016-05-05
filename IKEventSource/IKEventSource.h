77c24e1d0d52d6cd98c8e6c743cc16462a8332cf
Victor Barros <hey.zooi@gmail.com>
2016-05-05 11:41:42 -0400

    creating a new framework target and a shared scheme, so it can be used by Carthage package manager

---
+++ IKEventSource/IKEventSource.h
@@ -0,0 +1,19 @@
+//
+//  IKEventSource.h
+//  IKEventSource
+//
+//  Created by Victor Barros on 2016-05-05.
+//  Copyright © 2016 Inaka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IKEventSource.
+FOUNDATION_EXPORT double IKEventSourceVersionNumber;
+
+//! Project version string for IKEventSource.
+FOUNDATION_EXPORT const unsigned char IKEventSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IKEventSource/PublicHeader.h>
+
+
