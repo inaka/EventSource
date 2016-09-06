5473c4845eb82dd0b57dbbf96fe78165d55a6de3
Robbie Trencheny <me@robbiet.us>
2016-09-06 16:31:29 -0700

    more swift3 fixes

--- EventSourceTests/TestableEventSource.swift
+++ EventSourceTests/TestableEventSource.swift
@@ -11,7 +11,7 @@ import UIKit
 class TestableEventSource: EventSource {
 
 	func callDidReceiveResponse() {
-		let delegate = self as! URLSessionDataDelegate
+		let delegate = self as URLSessionDataDelegate
 
 		delegate.urlSession!(self.urlSession!, dataTask: self.task!, didReceive: URLResponse()) { (NSURLSessionResponseDisposition) -> Void in
 
@@ -19,19 +19,19 @@ class TestableEventSource: EventSource {
 	}
 
 	func callDidReceiveDataWithResponse(_ dataTask: URLSessionDataTask) {
-		let delegate = self as! URLSessionDataDelegate
+		let delegate = self as URLSessionDataDelegate
 		delegate.urlSession!(self.urlSession!, dataTask: dataTask, didReceive: "".data(using: String.Encoding.utf8)!)
 	}
 
 	func callDidReceiveData(_ data: Data) {
-		let delegate = self as! URLSessionDataDelegate
+		let delegate = self as URLSessionDataDelegate
 		delegate.urlSession!(self.urlSession!, dataTask: self.task!, didReceive: data)
 	}
 
 	func callDidCompleteWithError(_ error: String) {
 		let errorToReturn = NSError(domain: "Mock", code: 0, userInfo: ["mock":error])
 
-		let delegate = self as! URLSessionDataDelegate
+		let delegate = self as URLSessionDataDelegate
 		delegate.urlSession!(self.urlSession!, task: self.task!, didCompleteWithError: errorToReturn)
 
 	}
