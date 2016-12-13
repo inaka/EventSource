d5bd22e7d7d2040bab5569d340e04028828a108d
Jared Friese <jared.friese@gmail.com>
2016-12-13 06:18:32 -0800

    Remove boilerplate

--- EventSource/EventSource.swift
+++ EventSource/EventSource.swift
@@ -1,11 +1,3 @@
-//
-//  EventSource.swift
-//  EventSource
-//
-//  Created by Andres on 2/13/15.
-//  Copyright (c) 2015 Inaka. All rights reserved.
-//
-
 import Foundation
 
 public enum EventSourceState {
