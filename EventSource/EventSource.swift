599036fb5e0820cb388ff9f1218e68a23636271d
Håkon Bogen <bogen@beiningbogen.no>
2018-02-13 15:10:00 +0100

    Fix issue with strong reference making library contain itself and reconnecting even though it has been released

--- EventSource/EventSource.swift
+++ EventSource/EventSource.swift
@@ -183,8 +183,8 @@ open class EventSource: NSObject, URLSessionDataDelegate {
         if error == nil || (error! as NSError).code != -999 {
             let nanoseconds = Double(self.retryTime) / 1000.0 * Double(NSEC_PER_SEC)
             let delayTime = DispatchTime.now() + Double(Int64(nanoseconds)) / Double(NSEC_PER_SEC)
-            DispatchQueue.main.asyncAfter(deadline: delayTime) {
-                self.connect()
+            DispatchQueue.main.asyncAfter(deadline: delayTime) { [weak self] in
+                self?.connect()
             }
         }
 
